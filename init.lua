230ed95678af6b819223e75614599a025e1c6439
Zenon Seth <Zenon.Seth@gmail.com>
2023-11-02 16:09:40 +0000

    Add multiline message bubble support and splitting by whole word

--- init.lua
+++ init.lua
@@ -2,18 +2,54 @@ local shown = {}
 
 local settings = minetest.settings
 local MSG_BUBBLE_LIFETIME = tonumber(settings:get("message_bubbles_lifetime")) or 10
-local MAX_MSG_BUBBLE_LENGTH = tonumber(settings:get("message_bubbles_char_limit")) or 40
+local MAX_CHARS_PER_LINE = tonumber(settings:get("message_bubbles_char_limit")) or 40
+local MAX_NUM_LINES = tonumber(settings:get("message_bubbles_max_num_lines")) or 1
 local MSG_BUBBLE_PREFIX = settings:get("message_bubbles_prefix") or "Says: "
 
 local cumulativeTime = 0
 
 local function trim(msg)
   if not msg or not type(msg) == "string" then return "" end
-  if string.len(msg) > MAX_MSG_BUBBLE_LENGTH then
-    return string.sub(msg, 1, MAX_MSG_BUBBLE_LENGTH)..".."
-  else
-    return msg
+  if string.len(msg) <= MAX_CHARS_PER_LINE then return msg end
+  if MAX_NUM_LINES <= 1 then return string.sub(msg, 1, MAX_CHARS_PER_LINE)..".." end
+
+  -- else: we have multilines to split
+  local words = string.split(msg, " ", false)
+  local ret = ""
+  local currLine = 1
+  local wordIndex = 1
+  local line = ""
+  while currLine <= MAX_NUM_LINES do
+    local adjMaxChPerLine = MAX_CHARS_PER_LINE
+    if currLine == 1 then adjMaxChPerLine = MAX_CHARS_PER_LINE - string.len(MSG_BUBBLE_PREFIX) end
+    local space = ""
+    if string.len(line) > 0 then space = " " end
+    local tmp = line..space
+    if words[wordIndex] then
+      tmp = line..space..words[wordIndex]
+    else -- we used all the words, return early
+      currLine = MAX_NUM_LINES + 1
+      return ret..line
+    end
+
+    if string.len(tmp) <= adjMaxChPerLine then
+      -- adding this word didn't overflow the line length
+      line = tmp
+      wordIndex = wordIndex + 1
+    else -- the word pushed the line length over the limit
+      if currLine == MAX_NUM_LINES then
+        -- last line, just cut last word and return early
+        line = string.sub(tmp, 1, adjMaxChPerLine)
+        return ret..line..".."
+      else
+        -- don't add the word, leave it to the next line (don't increment wordIndex)
+        ret = ret..line.."\n"
+      end
+      line = ""
+      currLine = currLine + 1
+    end
   end
+  return ret
 end
 
 minetest.register_on_chat_message(function(name, origMessage)
